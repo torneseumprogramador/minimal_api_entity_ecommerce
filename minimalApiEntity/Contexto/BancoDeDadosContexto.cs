c41e9e1500eff96898de9cd7a6a71646a30dfbff
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-06-08 00:46:25 -0300

    Passando a dependencia do banco via construtor

--- minimalApiEntity/Contexto/BancoDeDadosContexto.cs
+++ minimalApiEntity/Contexto/BancoDeDadosContexto.cs
@@ -5,14 +5,26 @@ namespace Entity.Contexto;
 
 public class BancoDeDadosContexto : DbContext
 {
+    // #3 - Passando a dependencia do banco de dados via construtor
+    public BancoDeDadosContexto(DbContextOptions<BancoDeDadosContexto> options) : base(options)
+    {
+    }
+
+    public BancoDeDadosContexto() { }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        IConfigurationRoot configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
+        // #1 - Caso não tenha sido configurado na classe Program ele entra no if
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
 
-        optionsBuilder.UseNpgsql(configuration.GetConnectionString("conexao"));
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("conexao"));
+        }
     }
+
     public DbSet<Cliente> Clientes { get; set; } = default!;
 }
--- minimalApiEntity/Program.cs
+++ minimalApiEntity/Program.cs
@@ -1,9 +1,18 @@
+using Entity.Contexto;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// #2 - Configurando a conexão através de injeção de dependência
+builder.Services.AddDbContext<BancoDeDadosContexto>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("conexao"));
+});
 
 var app = builder.Build();
 
