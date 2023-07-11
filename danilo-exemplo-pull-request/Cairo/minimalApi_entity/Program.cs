2352b2a52f9b47f3bdc3fc88e50462b173aef516
Cairo Braz Costa <cairo.costa@animaeducacao.com.br>
2023-07-11 15:13:46 -0300

    cliente router

--- danilo-exemplo-pull-request/Cairo/minimalApi_entity/Program.cs
+++ danilo-exemplo-pull-request/Cairo/minimalApi_entity/Program.cs
@@ -1,4 +1,5 @@
 using Cairo.Contexto;
+using Cairo.Routes;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,30 +24,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+ClienteRouter.Register(app);
+//PedidoRouter.Register(app);
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
